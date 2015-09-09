b1315065c1d3e5a51b577225479822e0c8a2aa29
Marita Holm <maritafholm@gmail.com>
2015-09-09 13:12:38 +0200

    first commit

---
+++ sensor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sensor.h"
+
+int getNextData() {
+		static const char filename[] = "ECG.txt";
+		FILE *file = fopen(filename,"r");
+		int value;
+		fscanf(file,"%i",&value);
+
+		return value;
+}
