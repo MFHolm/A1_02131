4e2ec649d2f46aec83dc0cfcf07623a1c985820b
Marita Holm <maritafholm@gmail.com>
2015-09-09 17:53:51 +0200

    derivative works

--- filters.c
+++ filters.c
@@ -5,18 +5,36 @@
 void lowPass(int* input, int* output){
 	int result = 2*output[0]-output[1]+(((input[0]-2*input[6]+input[12]))/32);
 
-	printf("%d\n" , result);
-	insertArray(output,32,result);
+	//printf("%d\n %d\n %d\n%d\n%d\n%d\n%d\n%d\n" ,output[0], output[1], input[0], input[6],input[7],input[8], input[12], result);
+	//printf("%d\n", result);
+	insertArray(output,33,result);
 }
 
-void highPass(){
+void highPass(int* input, int* output){
+	int result = output[0] - input[0]/32 + input[16] - input[17] + input[32]/32;
+	//printf("%d\n", result);
+	insertArray(output,5,result);
+}
 
+int derivative(int* input) {
+	int result = (2*input[0]+input[1]-input[3]-2*input[4])/8;
+	//printf("%d\n", result);
+	return result;
+
+
+}
+
+void square(int input, int* output) {
+	int result = input * input;
+	insertArray(output, 30, result);
+	printf("%d\n", result);
 }
 
 
 void insertArray(int* x,int size, int new){
-	for(int i = 1; i < size; i++){
-		x[i]=x[i-1];
+	for(int i = size-1; i > 0; i--){
+		//printf("inserting %d on space %d\n", x[i-1], i);
+		x[i] = x[i-1];
 	}
 	x[0]=new;
 
--- main.c
+++ main.c
@@ -12,18 +12,24 @@ int main() {
 
 	int input[13] = {0};
 	int afterLowpass[33] = {0};
+	int afterHighpass[5] = {0};
+	int afterDerivative = 0;
+	int afterSquare[30] = {0};
 	//for (int i = 0; i < 12; i++) {
 	//	printf("%d\n", input[i]);
 	//}
 	int counter = 0;
 	int x;
 
-	while (!feof(file)){
-
+	do{
 		x = getNextData(file);
 		insertArray(input,13,x);
 		lowPass(input,afterLowpass);
-	}
+		highPass(afterLowpass,afterHighpass);
+		afterDerivative = derivative(afterHighpass);
+		printf("her");
+		square(afterDerivative);
+	 }while (!feof(file));
 
 
 
